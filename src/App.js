6bac0c8bd2869ca61b913c092834fe94596e8cb5
ali-203 <62180010+ali-203@users.noreply.github.com>
2021-02-12 13:58:17 +0500

    updateupdated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import "./App.css";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import AccessibilityIcon from '@material-ui/icons/Accessibility';
 
 function App() {
     return (
-        <div>
-            <h1>ali raza</h1>
-        </div>
+        <>
+<div className="back_img">
+    <div class="topnav">
+  <h3 className="active" href="#home">Legend!</h3>
+  <div class="topnav-right">
+  <a href="#contact">Portfoli</a>
+  <a href="#contact">News</a>
+  <a href="#contact">Services</a>
+    <a href="#search">Search</a>
+    <a href="#about">About</a>
+  </div>
+</div>
+<hr className="new4"/>
+<div className="legeng_text">
+    <h1>Legend!We make web a beautifull place</h1>
+    <form>
+    <p>Signup for our Newsletter to be updated</p>
+    
+    <input className="ali_raza" type="email" placeholder="ali raza @ gmail.com"/>
+    <input className="notify" type="submit" value="Notify me"/>
+    </form>
+</div>
+<div className="icons">
+<FacebookIcon />
+<AccessibilityIcon />
+</div>
+<div className="hedding">
+<h1>We work to make web a beautifull  place we craft beautifull design and convert then into fully functional and user friendly webapp </h1>
+
+</div>
+
+</div>
+        </>
     )
 }
 
